150195b576559d392af476917fbb4c4ab2217c90
Grazi Almeida <grazibatista2005@gmail.com>
2026-03-26 19:10:56 -0300

    Correcting variable identification

--- main.py
+++ main.py
@@ -14,19 +14,18 @@ if __name__ == "__main__":
     passo = methods.calcularPasso(a,b,num_trapezios)
     print(f"Passo (h) = {passo}\n")
 
-    x = sp.symbols('x') # X da matematica
-    f = sp.sympify(funcao)
+    f = sp.sympify(funcao, locals={'e': sp.E, 'pi': sp.pi})
 
-    soma = methods.calcularSomatorio(f,a,b,num_trapezios,num_casas_decimais)
+    soma = methods.calcularSomatorio(funcao,f,a,b,num_trapezios,num_casas_decimais)
     print()
     area = methods.calcularAreaTrapezio(a,b,soma,num_casas_decimais,num_trapezios)
     print()
     erro_arredondamento = methods.calcularErroDeArredondamento(a,b,num_trapezios,num_casas_decimais)
     print()
-    max_segunda_derivada = methods.maxSegundaDerivada(f,a,b,passo,num_casas_decimais,num_trapezios)
+    max_segunda_derivada = methods.maxSegundaDerivada(funcao,f,a,b,passo,num_casas_decimais,num_trapezios)
     print()
     erro_truncamento = methods.calcularErroDeTruncamento(passo,num_casas_decimais,max_segunda_derivada,num_trapezios)
     print()
     erro_total = methods.calcularErroTotal(erro_truncamento, erro_arredondamento,num_casas_decimais)
     print()
-    intervalo_final = methods.respostaFinal(f,a,b,area,num_casas_decimais,erro_total)
+    intervalo_final = methods.respostaFinal(funcao,f,a,b,area,num_casas_decimais,erro_total)
--- methods.py
+++ methods.py
@@ -1,11 +1,19 @@
 
 import sympy as sp
 
+def temVariavel(funcao:str) -> bool:
+    for item in funcao:
+        if item in ("x", "y", "z"):
+            return True
+    return False
+
+
 def calcularPasso(a:int, b:int, num_trapezios:int) -> float: #tambem é a altura do trapezio
     return (b - a) / num_trapezios
 
-def criarTabela(funcao,a:int, b:int,num_trapezios:int, casas_decimais:int) -> list:
+def criarTabela(f:str,funcao,a:int, b:int,num_trapezios:int, casas_decimais:int) -> list:
     passo = calcularPasso(a,b,num_trapezios)
+    possuiVariavel = temVariavel(f)
     logs = funcao.atoms(sp.log)
     lista_dos_resultados = list()
 
@@ -14,26 +22,38 @@ def criarTabela(funcao,a:int, b:int,num_trapezios:int, casas_decimais:int) -> li
             x_arg = log_expr.args[0]
             funcao = funcao.subs(log_expr, sp.log(x_arg, 10))
 
-    var = list(funcao.free_symbols)[0]
-    math_function = sp.lambdify(var,funcao)
+    if possuiVariavel:
+        var = list(funcao.free_symbols)[0]
+        math_function = sp.lambdify(var,funcao)
+    else:
+        math_function = sp.lambdify([],funcao)
     
     #separar entre x e f(x) visualmente
     print(f"{'x':>3} {'|':>5} {'f(x)':>7}")
     print('-' * 15)
 
-    while a <= b:
-       resultado = math_function(a)
-       print(f"{a:.1f}\t  {resultado:.{casas_decimais}f}")
-       lista_dos_resultados.append(resultado)
-       a += passo
+    if possuiVariavel:
+        while a <= b:
+            resultado = math_function(a)
+            print(f"{a:.1f}\t  {resultado:.{casas_decimais}f}")
+            lista_dos_resultados.append(resultado)
+            a += passo
+    else:
+        while a <= b:
+            resultado = math_function()
+            print(f"{a:.1f}\t  {resultado:.{casas_decimais}f}")
+            lista_dos_resultados.append(resultado)
+            a += passo
+    
+   
        
     print('-' * 15)
 
     return lista_dos_resultados
 
 
-def calcularSomatorio(funcao,a:int, b:int,num_trapezios:int, casas_decimais:int)->float:
-    resultados = criarTabela(funcao,a,b,num_trapezios,casas_decimais)
+def calcularSomatorio(f,funcao,a:int, b:int,num_trapezios:int, casas_decimais:int)->float:
+    resultados = criarTabela(f,funcao,a,b,num_trapezios,casas_decimais)
     n = len(resultados)
     soma = 0
     equacao_soma_areas = [] #pra printar a soma das areas, primeiro e utlimo dividio por 2 
@@ -69,30 +89,25 @@ def calcularErroDeArredondamento(a:int,b:int,num_trapezios:int,casas_decimais:in
 # TODO corrigir o calculo do max da segunda derivada - acho que ok
 # TODO imprimir: passo, equacao do erro de arredondamento, formula da area - ok
 
-def maxSegundaDerivada(funcao,a:int,b:int,passo:float,casas_decimais:int, num_trapezios:int) -> float:
+def maxSegundaDerivada(f:str,funcao,a:int,b:int,passo:float,casas_decimais:int, num_trapezios:int) -> float:
     valor_max = 0
-
+    possuiVariavel = temVariavel(f)
     logs = funcao.atoms(sp.log)
     for log_expr in logs: #caso a expressao tenha log, transforma o log para base 10
         if len(log_expr.args) == 1:
             x_arg = log_expr.args[0]
             funcao = funcao.subs(log_expr, sp.log(x_arg, 10))
-    var = list(funcao.free_symbols)[0]
 
-    segunda_derivada = sp.diff(funcao, var, 2)
-    segunda_derivada = sp.lambdify(var, segunda_derivada)
+    if possuiVariavel:
+        var = list(funcao.free_symbols)[0]  # pick a variable
+        segunda_derivada_sym = sp.diff(f, var, 2)  # differentiate SymPy expression
+        segunda_derivada = sp.lambdify(var, segunda_derivada_sym)
 
-
-    # while a <= b:
-    #     val = abs(segunda_derivada(a))
-    #     if val > valor_max:
-    #         valor_max  = val
-    #     a += passo
-    for i in range(num_trapezios+1): #exemplo intervalo 2 a 5, 6 trapezios = 0 a 6 pontos, 7 pontos
-        x = a + i*passo #vai testar cada ponto (usei o ex 2 a 5, passo = 0.5, pontos: 2, 2.5, 3, 3.5, 4, 4.5, 5)
-        val = abs(segunda_derivada(x))
-        if val > valor_max:
-            valor_max  = val
+        for i in range(num_trapezios+1): #exemplo intervalo 2 a 5, 6 trapezios = 0 a 6 pontos, 7 pontos
+            x = a + i*passo #vai testar cada ponto (usei o ex 2 a 5, passo = 0.5, pontos: 2, 2.5, 3, 3.5, 4, 4.5, 5)
+            val = abs(segunda_derivada(x))
+            if val > valor_max:
+                valor_max  = val
     
     return valor_max 
 
@@ -109,9 +124,12 @@ def calcularErroTotal(Etru:float, Earr:float,casas_decimais:int) -> float:
     print(f"|Etot| <= |Ea| + |Etru| <= {Earr} + {Etru} < {Earr + Etru} ≈ {erro:.{casas_decimais}f} (com {casas_decimais} casas)")
     return erro
 
-def respostaFinal(funcao,a:int,b:int,area:float,casas_decimais:int,Etot):
-    x = list(funcao.free_symbols)[0]
-    funcao= sp.Integral(funcao, (x, a, b))
+def respostaFinal(f:str,funcao,a:int,b:int,area:float,casas_decimais:int,Etot):
+    if temVariavel(f):
+        x = list(funcao.free_symbols)[0]
+        funcao= sp.Integral(funcao, (x, a, b))
+    else:
+       funcao = funcao 
     inferior = area - Etot
     superior = area + Etot  
     sp.pprint(funcao)
